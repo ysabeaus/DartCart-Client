385cfae930fe8cf35770ad856d4a6a226b65094e
David427 <david427@live.com>
2022-03-01 21:54:11 -0500

    Redux refactor initial commit

---
+++ dartcart-client/src/common/store.ts
@@ -0,0 +1,23 @@
+import { configureStore } from "@reduxjs/toolkit";
+import accountSlice from "./accountSlice";
+import { accountApi } from "../services/account";
+
+// Here we configure the store object that redux uses for storing data
+// Each slice's reducer is added as a reducer here. Note that redux
+// toolkit query's createApi is creating a slice as well, so it is included
+//
+// we also declare all the middleware we'll be using (here we add the exampleApi
+// to the default middleware that comes with RTK)
+const store = configureStore({
+  reducer: {
+    account: accountSlice,
+    [accountApi.reducerPath]: accountApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(accountApi.middleware),
+});
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
---
+++ dartcart-client/src/common/types.ts
@@ -0,0 +1,17 @@
+export interface Product {
+    id: number,
+    name: string,
+    ordered: boolean
+}
+
+export interface User {
+    id: number,
+    username: string,
+    password: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    phone: string,
+    location: string,
+    registrationDate: number
+}
