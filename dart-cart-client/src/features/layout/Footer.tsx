b6cc51623617fe19ebbdc360c0593f2531d9e4c6
Art Weinstein <artur.weinstein@gmail.com>
2022-03-04 13:39:25 -0800

    move search bar to cetner of header and fix logo link

--- dart-cart-client/src/features/layout/Footer.tsx
+++ dart-cart-client/src/features/layout/Footer.tsx
@@ -60,15 +60,14 @@ export default function Footer () {
         <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
 
           <h6 className="text-uppercase fw-bold mb-4">
-           
+            About
           </h6>
-          <p><i className="fas fa-home me-3"></i> New York, NY 10012, US</p>
+          <p>Reston, VA 20190, US</p>
           <p>
-            <i className="fas fa-envelope me-3"></i>
-            info@example.com
+            questions@dartcart.com
           </p>
-          <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
-          <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
+          <p>+ 01 234 567 88</p>
+          <p>+ 01 234 567 89</p>
         </div>
 
       </div>
@@ -76,8 +75,7 @@ export default function Footer () {
   </section>
 
   <div className="text-center p-4" style={{backgroundColor: "rgba(0, 0, 0, 0.05)"}}>
-    © 2021 Copyright:
-    <a className="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    © 2022 Dart Cart
   </div>
   
 </footer>
--- dart-cart-client/src/features/layout/Header.tsx
+++ dart-cart-client/src/features/layout/Header.tsx
@@ -13,52 +13,53 @@ const Header = () => {
     <>
       <nav className="navbar navbar-expand-lg navbar-fixed-top navbar-custom">
         <div className="container-fluid">
-          <div className="navbar-header image-container">
-            <a href="Display">
-              <img className="logo-img" style={logoStyle} src={logo} alt="logo"></img>
-            </a>
-          </div>
-          <div className="navbar-header link-container">
-            <ul className="navbar-nav mr-auto link-container">
-              <li className="nav-item active"><a className="nav-link" href="/">Home</a></li>
-              <li className="nav-item"><a className="nav-link" href="login">Login</a></li>
-              <li className="nav-item dropdown"><a className="nav-link" href="register">Register</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">Explore</a></li>
-            </ul>
-          </div>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="navbar-brand navbar-right">
-            <form className="form-inline my-2 my-lg-0">
-              <input type="submit" className="btn btn-success" value="Search" style={{ float: 'right', backgroundColor: '#198754' }} />
-              <div style={{ overflow: 'hidden', paddingRight: '.5em' }}>
-                <input type="text" placeholder="Search" style={{ width: '100%' }} />
-              </div>
-            </form>
-          </div>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          <div style={{ textAlign: 'left'}}>
+          <a href="Display">
+            <img className="logo-img" style={logoStyle} src={logo} alt="logo"></img>
+          </a>
         </div>
-      </nav>
+        
+        <div className="navbar-brand" style={{ width: '60%', marginLeft: '20px' }}>
+          <form className="form-inline my-2 my-lg-0">
+            <input type="submit" className="btn btn-success" value="Search" style={{ float: 'right', backgroundColor: '#198754' }} />
+            <div style={{ overflow: 'hidden', paddingRight: '.5em' }}>
+              <input type="text" placeholder="Search" style={{ width: '100%' }} />
+            </div>
+          </form>
+        </div>
+
+        <div className="navbar-header">
+          <ul className="navbar-nav mr-auto link-container" style={{ textAlign: 'center' }}>
+            <li className="nav-item active"><a className="nav-link" href="/">Home</a></li>
+            <li className="nav-item"><a className="nav-link" href="login">Login</a></li>
+            <li className="nav-item"><a className="nav-link" href="register">Register</a></li>
+            <li className="nav-item"><a className="nav-link" href="#">Explore</a></li>
+          </ul>
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </nav>
     </>
   );
 };
