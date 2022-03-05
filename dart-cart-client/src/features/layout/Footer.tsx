c0f113154906cd052bf9dc8172d0799cbaf6b799
Jack <iatenine@gmail.com>
2022-03-05 10:53:18 -0700

    Header and Footer components fully modularized
    Modularization has been fully applied to both the Header and Footer have had all business logic abstracted away into different components,
    leaving them as parent components that can singularly focus on ordering of components and Bootstrap column management

--- dart-cart-client/src/features/layout/Footer.tsx
+++ dart-cart-client/src/features/layout/Footer.tsx
@@ -1,16 +1,16 @@
-// import { Navbar } from "react-bootstrap";
 import Navbar from "./Navbar";
-import { Link } from "react-router-dom";
 import About from "./About";
 import SocialMedia from "./SocialMedia";
+import UsefulLinks from "./UsefulLinks";
 
 export default function Footer() {
+  // divs dealing with managing columns were not abstracted away so this area can focus on applying styles correctly
+
   return (
     <footer className="text-center text-lg-start bg-black">
       <section className="footer-info">
         <div className="container text-center text-md-start mt-5">
           <div className="row mt-3">
-            {/* Social Media Links */}
             <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
               <SocialMedia />
             </div>
@@ -20,32 +20,11 @@ export default function Footer() {
               <Navbar footer="true" />
             </div>
 
-            {/* Useful Links */}
             <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
               <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
-              <p>
-                <a href="#!" className="text-reset">
-                  Sell on Dart Cart
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Returns
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Orders
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Help
-                </a>
-              </p>
+              <UsefulLinks />
             </div>
 
-            {/* About*/}
             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
               <About />
             </div>
--- dart-cart-client/src/features/layout/UsefulLinks.tsx
+++ dart-cart-client/src/features/layout/UsefulLinks.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const UsefulLinks = () => {
-  return <div>UsefulLinks</div>;
+  const links = {
+    "Sell on Dart Cart": "#",
+    Returns: "#",
+    Orders: "#",
+    Help: "#",
+  };
+
+  const navLinks = Object.keys(links).map((link) => {
+    return (
+      <p>
+        <Link to={links[link]} className="text-reset">
+          {link}
+        </Link>
+      </p>
+    );
+  });
+
+  return <>{navLinks}</>;
 };
 
 export default UsefulLinks;
