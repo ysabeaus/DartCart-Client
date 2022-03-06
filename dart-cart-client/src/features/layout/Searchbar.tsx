15ff4bcf3f7968bbe2f9b42b3f21d71bb3c91fe6
Art Weinstein <artur.weinstein@gmail.com>
2022-03-05 16:45:39 -0800

    style navbar and social media icons

--- dart-cart-client/src/features/layout/Searchbar.tsx
+++ dart-cart-client/src/features/layout/Searchbar.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-
+import Categories from "./Categories"
+import { Navbar } from 'react-bootstrap';
 const Searchbar = () => {
   return (
+    
     <div className="navbar-brand" style={{ width: "60%", marginLeft: "20px" }}>
       <div className="form-inline my-2 my-lg-0">
         <input
@@ -9,12 +11,13 @@ const Searchbar = () => {
           className="btn btn-success"
           value="Search"
           style={{ float: "right", backgroundColor: "#198754" }}
-        />
+          />
         <div style={{ overflow: "hidden", paddingRight: ".5em" }}>
           <input type="text" placeholder="Search" style={{ width: "100%" }} />
         </div>
       </div>
     </div>
+    
   );
 };
 
