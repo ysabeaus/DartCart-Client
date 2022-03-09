f751e8fc696aa854857524c4eab318be6d234ddc
Jack <iatenine@gmail.com>
2022-03-09 15:47:40 -0700

    ShopProductCard modified

--- dart-cart-client/src/features/product-details/ShopProductCard.tsx
+++ dart-cart-client/src/features/product-details/ShopProductCard.tsx
@@ -9,6 +9,7 @@ interface IShopProductCard {
 const ComputerUrl =
   "https://images.unsplash.com/photo-1587831990711-23ca6441447b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZGVza3RvcCUyMGNvbXB1dGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80";
 
+  
 export function ShopProductCard({ Product }: IShopProductCard) {
   return (
     <>
