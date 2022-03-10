87e9134e8e18548ba3604741a6d79dd2a97cd7f7
David Melanson Jr <david427@live.com>
2022-03-09 21:31:31 -0500

    Update footer links

---
+++ dart-cart-client/src/features/authentication/SellerAccess.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SellerAccess = () => {
+    return <div>SellerAccess</div>;
+};
+
+export default SellerAccess;
--- dart-cart-client/src/features/layout/Footer.tsx
+++ dart-cart-client/src/features/layout/Footer.tsx
@@ -2,42 +2,40 @@ import Navbar from "./Navbar";
 import About from "./About";
 import SocialMedia from "./SocialMedia";
 import UsefulLinks from "./UsefulLinks";
+import FooterNav from "./FooterNav";
 
 export default function Footer() {
-  // divs dealing with managing columns were not abstracted away so this area can focus on applying styles correctly
+    // divs dealing with managing columns were not abstracted away so this area can focus on applying styles correctly
 
-  return (
-    <footer className="text-center text-lg-start bg-black">
-      <section className="footer-info">
-        <div className="container text-center text-md-start mt-5">
-          <div className="row mt-3">
-            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              <SocialMedia />
+    return (
+        <footer className="text-center text-lg-start bg-black">
+            <section className="footer-info">
+                <div className="container text-center text-md-start mt-5">
+                    <div className="row mt-3">
+                        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                            <SocialMedia />
+                        </div>
+
+                        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <h6 className="text-uppercase fw-bold mb-4">Navigation</h6>
+                            <FooterNav />
+                        </div>
+
+                        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+                            <UsefulLinks />
+                        </div>
+
+                        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                            <About />
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <div className="text-center p-4" style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}>
+                © 2022 Dart Cart
             </div>
-
-            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Navigation</h6>
-              <Navbar footer="true" />
-            </div>
-
-            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
-              <UsefulLinks />
-            </div>
-
-            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-              <About />
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <div
-        className="text-center p-4"
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
-      >
-        © 2022 Dart Cart
-      </div>
-    </footer>
-  );
+        </footer>
+    );
 }
