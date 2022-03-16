55e96df0887e205b5988581e7a3b23f9749ad9a8
Awaab Elamin <AwaabElamin@gmail.com>
2022-03-16 16:51:14 -0500

    add a link to each feature Product to open all product destilas in its own page

--- dart-cart-client/src/Models/Product.tsx
+++ dart-cart-client/src/Models/Product.tsx
@@ -1,8 +1,19 @@
 import { useParams } from "react-router-dom";
 const obj = [
     {
-        id:1,
-        image: ''
+        id: 1,
+        image: 'https://picsum.photos/200/200?random=1',
+        productName: 'test1'
+    },
+    {
+        id: 2,
+        image: 'https://picsum.photos/200/200?random=2',
+        productName: 'test2'
+    },
+    {
+        id: 3,
+        image: 'https://picsum.photos/100/100?random=3',
+        productName: 'test3'
     }
 ];
 export default function Product() {
@@ -15,15 +26,34 @@ export default function Product() {
         backgroundRepeat: "no-repeat",
         width: "40%",
     };
+    let pro = {
+        id: 1,
+        image: 'https://picsum.photos/10/10?random=1',
+        productName: 'test1'
+    };
+
+    for (let i = 0; i < obj.length; i++) {
+        if (obj[i].id == id) {
+            pro.id = obj[i].id;
+            pro.image = obj[i].image;
+            pro.productName = obj[i].productName;
+            break;
+        }
+    }
     return (<div className="ProductContainer">
         <div className="InnerProduct">
-            <div className="ProductInfoContainer">
-                <div style={ImgStyleBase}></div>
-                <div className="ProductInfoPocket">
-                    <h1> {product_id}</h1>
-                </div>
 
+            <div className="ProductInfoContainer" >
+                <img className="card-img-top" style={{ width: "18rem" }}
+                    src={pro.image}
+                    alt="Card image cap"
+                ></img>
+                <div className="ProductInfoPocket">
+                    <h1> id = {pro.id}</h1>
+                    <h1>{pro.productName}</h1>
+                </div>
             </div>
+
         </div>
     </div>
     )
