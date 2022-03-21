a40c3fa522fe8af0520c0fb6289f63d33c6f29ed
Philip Wentworth <guitartheist@gmail.com>
2022-03-21 16:40:57 -0400

    add inventory feature in progress

--- dart-cart-client/src/App.tsx
+++ dart-cart-client/src/App.tsx
@@ -6,6 +6,7 @@ import { ResetPassword } from "./features/authentication/ResetPassword";
 import Home from "./features/Home";
 import UserRegister from "./features/user-register/UserRegister";
 import ProductRegister from "./features/product-register/ProductRegister";
+import AddInventory from "./features/shop-product-add/AddInventory";
 import Display from "./features/display/Display";
 import PreviousOrders from "./features/previous-orders/previous-orders";
 import SellerRegister from "./features/seller-register/SellerRegister";
@@ -61,6 +62,7 @@ function App() {
             <Route path="/cart" element={<Cart />}></Route>
             <Route path="/register" element={<UserRegister />}></Route>
             <Route path="/product-register" element={<ProductRegister />}></Route>
+            <Route path="/shop-product-add" element={<AddInventory />}></Route>
             <Route path="/signup" element={<SellerRegister />}></Route>
             <Route path="/login" element={<Login />}></Route>
             <Route path="/ResetPassword" element={<ResetPassword />}></Route>
