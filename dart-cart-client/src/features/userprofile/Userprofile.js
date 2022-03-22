cc56513e7388452a4b2f3cb2930301d4f4873acd
JSotto09 <jacsotto09@gmail.com>
2022-03-22 16:23:38 -0400

    added userprofile fixing merges

---
+++ dart-cart-client/src/features/userprofile/Userprofile.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { Container, Paper } from '@mui/material';
+import axios from "axios";
+import authHeader from "../../features/authentication/AuthHeader";
+import { orange, red } from '@mui/material/colors';
+
+
+function UserProfile(){
+
+    const[user, setUser] = useState({});
+
+    async function fetchUser() {
+        
+        const response = await fetch("http://localhost:9005/getProfile", { headers: authHeader() });
+        const fetchedUser = await response.json();
+        console.log(fetchedUser);
+        setUser(fetchedUser);
+    }
+
+  
+    useEffect(() => {
+      fetchUser();
+  }, []);
+
+  return(
+
+    <>
+          <Paper elevation={6} style={{margin:"10px" , padding:"15px", textAlign:"left"}} key={user.user_id}>
+          <h2>About Me</h2>
+            talk about yourself here
+          </Paper>
+          <Paper elevation={6} style={{margin:"10px" , padding:"15px", textAlign:"left"}} key={user.user_id}>
+            <h2>Additional information</h2>
+            First Name: {user.firstName}<br/>
+            Last Name: {user.lastName}<br/>
+            Email: {user.email}<br/>
+            Phone: {user.phone}
+
+          </Paper>
+
+          
+</>
+
+
+
+  )
+
+
+
+}
+
+export default UserProfile;
