e8990331ec82acb1f4f0d27034615c2287b808f2
Austin Blandford <ablandford2399@gmail.com>
2022-03-25 14:05:31 -0700

    Reverted some current changes to get feature back into working shape.

--- dart-cart-client/src/features/wishlist/WishListItemView.tsx
+++ dart-cart-client/src/features/wishlist/WishListItemView.tsx
@@ -1,76 +1,39 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "../../common/types";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from "../../common/types"
 import { WishListItem } from "../../common/types";
-import {
-  selectWishListItemById,
-  fetchWishList,
-  deleteFromWishList,
-} from "../../common/slices/wishlistSlice";
+import { selectWishListItemById, fetchWishList, deleteFromWishList } from "../../common/slices/wishlistSlice";
 import { Link } from "react-router-dom";
-import axios from "axios";
-
 
 const WishListItemView = ({ wishListId }: WishListItem) => {
-  const dispatch = useDispatch();
-  const item = useSelector((state: RootState) =>
-    selectWishListItemById(state, wishListId)
-  );
 
-  useEffect(() => {
-    dispatch(fetchWishList());
-  }, []);
+    const dispatch = useDispatch();
+    const item = useSelector((state: RootState) => selectWishListItemById(state, wishListId));
 
-  const sendDelete = async (productId) => {
-    await dispatch(deleteFromWishList(productId));
-    setTimeout(refresh, 500);
-  };
+    useEffect(() => {
+        dispatch(fetchWishList())
+    }, [])
 
-  const refresh = () => {
-    window.location.reload();
-  };
+    const sendDelete = async (productId) => {
+        await dispatch(deleteFromWishList(productId));
+        setTimeout(refresh, 500);
+    }
 
-  const getImageLink = async (productName) => {
-    let url;
+    function refresh() {
+        window.location.reload();
+    }
 
-    const response = await axios.get('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI', {
-        params: {
-            q: productName,
-            pageNumber: '1',
-            pageSize: '10',
-            autoCorrect: 'true'
-        },
-        headers: {
-            'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com',
-            'X-RapidAPI-Key': '28b5780197msh7b5254876e8e275p19653ajsnd85e31357767'
-        }
-    })
-
-    console.log(response);
-
-    return url;
-  };
-
-  return (
-    <div className="productContainer">
-      <img className="productImg" src={getImageLink(item?.product.name)} />
-      <section className="wishListBody">
-        <h5 className="productName">{item?.product.name}</h5>
-        <Link
-          to={`/shop-product/${item?.product.id}`}
-          className="btn viewProductBtn"
-        >
-          View Product
-        </Link>
-        <div
-          className="btn removeWishBtn"
-          onClick={() => sendDelete(item?.product.id)}
-        >
-          Remove from Wishlist
+    return(
+        <div className="productContainer">
+            <img className="productImg" src="https://www.russorizio.com/wp-content/uploads/2016/07/ef3-placeholder-image.jpg"/>
+            <section className="wishListBody">
+                <h5 className="productName">{ item?.product.name }</h5>
+                <Link to={`/shop-product/${ item?.product.id }`} className="btn viewProductBtn">View Product</Link>
+                <div className="btn removeWishBtn" onClick={ () => sendDelete(item?.product.id) }>Remove from Wishlist</div>
+            </section>
         </div>
-      </section>
-    </div>
-  );
-};
+    )
+
+}
 
 export default WishListItemView;
