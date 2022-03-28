66b48dcc8c9b0a3525f238db2e710920dbb3ded5
Austin Blandford <ablandford2399@gmail.com>
2022-03-28 09:29:21 -0700

    Updated css of wishlist slightly to make images smaller. Also have mostly implemented the rapidapi image search, but it still has some quirks. Only one image is getting loaded while every other image search comes back with a 429 error.

--- dart-cart-client/src/features/wishlist/WishListItemView.tsx
+++ dart-cart-client/src/features/wishlist/WishListItemView.tsx
@@ -1,27 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from "../../common/types"
 import { WishListItem } from "../../common/types";
 import { selectWishListItemById, fetchWishList, deleteFromWishList } from "../../common/slices/wishlistSlice";
 import { Link } from "react-router-dom";
+import productRegisterSlice from "../../common/slices/productRegisterSlice";
+import axios, { AxiosRequestConfig } from "axios";
 
 const WishListItemView = ({ wishListId }: WishListItem) => {
 
     const dispatch = useDispatch();
     const item = useSelector((state: RootState) => selectWishListItemById(state, wishListId));
+    const [image, setImage] = useState("https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png");
 
     useEffect(() => {
         dispatch(fetchWishList())
+        getImage(item?.product.name)
     }, [])
+
+    const getImage = async (productName) => {
+        console.log(productName);
+
+        const options: AxiosRequestConfig = {
+            method: 'GET',
+            url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI',
+            params: {
+                q: productName, 
+                pageNumber: '1', 
+                pageSize: '10', 
+                autoCorrect: 'true'},
+            headers: {
+              'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com',
+              'X-RapidAPI-Key': '28b5780197msh7b5254876e8e275p19653ajsnd85e31357767'
+            }
+        };
+
+        axios.request(options)
+            .then((response) => {
+                console.log(response.data)
+                setImage(response.data.value[0].url)
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
 
     const sendDelete = async (productId) => {
         await dispatch(deleteFromWishList(productId));
         dispatch(fetchWishList());
     }
 
+    // getImage(item?.product.name);
+
     return(
         <div className="productContainer">
-            <img className="productImg" src="https://www.russorizio.com/wp-content/uploads/2016/07/ef3-placeholder-image.jpg"/>
+            <img className="productImg" src={ image }/>
             <section className="wishListBody">
                 <h5 className="productName">{ item?.product.name }</h5>
                 <Link to={`/shop-product/${ item?.product.id }`} className="btn viewProductBtn">View Product</Link>
